51fbc8407d22409ec87251908ebb121b97662910
EveNyarango <evenyarango@gmail.com>
2020-12-05 15:27:41 +0300

    add getters and constructors

--- src/main/java/models/sightings.java
+++ src/main/java/models/sightings.java
@@ -1,4 +1,25 @@
 package models;
 
 public class sightings {
+    private int id;
+    private String location;
+    private String rangerName;
+
+    public sightings(String location, String rangerName) {
+        this.id = id;
+        this.location = location;
+        this.rangerName = rangerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getRangerName() {
+        return rangerName;
+    }
 }

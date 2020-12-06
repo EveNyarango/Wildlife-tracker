9d50006d20dfe8a189abffd6348288cf2df19f7e
EveNyarango <evenyarango@gmail.com>
2020-12-06 14:10:25 +0300

    add one route in app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,29 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.staticFileLocation;
+
 public class App {
+    public static void main (String[] args){
+        staticFileLocation("/public");
+
+//        homepage
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+
+
+
+
+
+
+
+    }
+
 }
